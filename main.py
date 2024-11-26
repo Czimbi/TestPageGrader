17d31a08514b680a04879526a80d12ccd3312fdc
Oliver Czimbalmos <oliver@Olivers-MacBook-Pro.local>
2024-11-26 20:57:23 +0100

    Finding the answer boxes on the image

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import random
 import gradio as gr
+from ShapeDetect import ShapeDetect
 
 
 def show_images(images, kill_later=True):
@@ -14,113 +15,193 @@ def show_images(images, kill_later=True):
     if kill_later:
         cv2.destroyAllWindows()
 
-image_path ="images/as-123-1.png"
+def find_contours(image) -> list:
+    """Gets the contours of an image
 
-ANSWER_KEY = {
-    0: 1,
-    1: 4,
-    2: 0,
-    3: 3
-}
+    Args:
+        image (np.ndarray): Image array
 
-# edge detection
-image = cv2.imread(image_path)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-edged = cv2.Canny(blurred, 75, 200)
-show_images([edged])
+    Returns:
+        list: contour coordinates
+    """
+    image = image.copy()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.Canny(blurred, 30, 30)
+    #Finding contours
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    return cnts
 
-# find contours in edge detected image
-cnts = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-docCnt = None
+def get_rectangles(cnts) -> list:
+    """Get rectangels on the image
 
-allContourImage = image.copy()
-cv2.drawContours(allContourImage, cnts, -1, (0, 0, 255), 3)
-print("Total contours found after edge detection {}".format(len(cnts)))
-show_images([allContourImage])
-
-# finding the document contour
-if len(cnts) > 0:
-    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-
+    Args:
+        cnts (list): contours
+    Returns:
+        (list): Contours of the rectangles
+    """
+    shape_detector = ShapeDetect()
+    # loop over the contours
+    possible_boxes = []
     for c in cnts:
-        peri = cv2.arcLength(c, closed=True)
-        approx = cv2.approxPolyDP(c, epsilon=peri*0.02, closed=True)
+        #Get if its a candiadte shape or not
+        shape = shape_detector.get_shape(c)
+        # then draw the contours and the name of the shape on the image
+        if shape == "Rectangle":
+            c = c.astype("int")
+            possible_boxes.append(c)
 
-        if len(approx) == 4:
-            docCnt = approx
-            break
+    return possible_boxes
 
-contourImage = image.copy()
-cv2.drawContours(contourImage, [docCnt], -1, (0, 0, 255), 2)
-show_images([contourImage])
+def get_answer_boxes(contours):
+    """Finds the answer boxes on the image.
 
-# Getting the bird's eye view, top-view of the document
-paper = four_point_transform(image, docCnt.reshape(4, 2))
-warped = four_point_transform(gray, docCnt.reshape(4, 2))
-show_images([paper, warped])
+    Args:
+        contours (list): List of contours on the image
+    Returns:
+        (list): list of contours
+    """
+    max_area = -1
+    answer_boxes = []
+    #Find the 
+    for c in contours:
+        #Create a rectangle and calculate the area
+        x, y, w, h = cv2.boundingRect(c)
+        if w * h > max_area:
+            max_area = w * h
+    #Finding the largest contour boxes
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        if w * h >= max_area * 0.90:
+            answer_boxes.append(c)
+    return answer_boxes
 
 
-# Thresholding the document
-thresh = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-show_images([thresh])
+if '__main__' == __name__:
 
-# Finding contours in threshold image
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-print("Total contours found after threshold {}".format(len(cnts)))
-questionCnts = []
+    image_path ="images/as-1234-1.png"
 
-allContourImage = paper.copy()
-cv2.drawContours(allContourImage, cnts, -1, (0, 0, 255), 3)
-show_images([allContourImage])
+    ANSWER_KEY = {
+        0: 1,
+        1: 4,
+        2: 0,
+        3: 3
+    }
 
-# Finding the questions contours
-for c in cnts:
-    (x, y, w, h) = cv2.boundingRect(c)
-    ar = w / float(h)
+    # edge detection
+    image = cv2.imread(image_path)
+    #Finding the contour
+    cnts = find_contours(image) 
+    #Getting the answer boxes
+    possible_boxes = get_rectangles(cnts)
+    answer_boxes = get_answer_boxes(possible_boxes)
 
-    if w >= 20 and h >= 20 and ar >= 0.9 and ar <= 1.1:
-        questionCnts.append(c)
+    #Drawing the answer boxes
+    images = []
+    for c in answer_boxes:
+        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+        images.append(image)
+    show_images(images)
 
-print("Total questions contours found: {}".format(len(questionCnts)))
+    # # edge detection
+    # image = cv2.imread(image_path)
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    # edged = cv2.Canny(blurred, 75, 200)
+    # show_images([edged])
 
-questionsContourImage = paper.copy()
-cv2.drawContours(questionsContourImage, questionCnts, -1, (0, 0, 255), 3)
-show_images([questionsContourImage])
+    # # find contours in edge detected image
+    # cnts = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # cnts = imutils.grab_contours(cnts)
+    # docCnt = None
 
-# Sorting the contours according to the question
-questionCnts = contours.sort_contours(questionCnts, method="top-to-bottom")[0]
-correct = 0
-questionsContourImage = paper.copy()
+    # allContourImage = image.copy()
+    # cv2.drawContours(allContourImage, cnts, -1, (0, 0, 255), 3)
+    # print("Total contours found after edge detection {}".format(len(cnts)))
+    # show_images([allContourImage])
 
-for (q, i) in enumerate(np.arange(0, len(questionCnts), 5)):
-    cnts = contours.sort_contours(questionCnts[i: i+5])[0]
-    cv2.drawContours(questionsContourImage, cnts, -1, (255,0,0), 2)
-    bubbled = None
+    # # finding the document contour
+    # if len(cnts) > 0:
+    #     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 
-    for (j, c) in enumerate(cnts):
-        mask = np.zeros(thresh.shape, dtype="uint8")
-        cv2.drawContours(mask, [c], -1, 255, -1)
+    #     for c in cnts:
+    #         peri = cv2.arcLength(c, closed=True)
+    #         approx = cv2.approxPolyDP(c, epsilon=peri*0.02, closed=True)
 
-        mask = cv2.bitwise_and(thresh, thresh, mask=mask)
-        #show_images([mask])
-        total = cv2.countNonZero(mask)
+    #         if len(approx) == 4:
+    #             docCnt = approx
+    #             break
 
-        if bubbled is None or total > bubbled[0]:
-            bubbled = (total, j)
+    # contourImage = image.copy()
+    # cv2.drawContours(contourImage, [docCnt], -1, (0, 0, 255), 2)
+    # show_images([contourImage])
 
-    color = (0, 0, 255)
-    k = ANSWER_KEY[q]
+    # # Getting the bird's eye view, top-view of the document
+    # paper = four_point_transform(image, docCnt.reshape(4, 2))
+    # warped = four_point_transform(gray, docCnt.reshape(4, 2))
+    # show_images([paper, warped])
 
-    if k == bubbled[1]:
-        color = (0, 255, 0)
-        correct += 1
 
-    cv2.drawContours(paper, [cnts[k]], -1, color, 3)
+    # # Thresholding the document
+    # thresh = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    # show_images([thresh])
 
-score = (correct / 5.0) * 100
-print("Score: {:.2f}%".format(score))
-cv2.putText(paper, "{:.2f}%".format(score), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
-show_images([image, paper])
+    # # Finding contours in threshold image
+    # cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # cnts = imutils.grab_contours(cnts)
+    # print("Total contours found after threshold {}".format(len(cnts)))
+    # questionCnts = []
+
+    # allContourImage = paper.copy()
+    # cv2.drawContours(allContourImage, cnts, -1, (0, 0, 255), 3)
+    # show_images([allContourImage])
+
+    # # Finding the questions contours
+    # for c in cnts:
+    #     (x, y, w, h) = cv2.boundingRect(c)
+    #     ar = w / float(h)
+
+    #     if w >= 20 and h >= 20 and ar >= 0.9 and ar <= 1.1:
+    #         questionCnts.append(c)
+
+    # print("Total questions contours found: {}".format(len(questionCnts)))
+
+    # questionsContourImage = paper.copy()
+    # cv2.drawContours(questionsContourImage, questionCnts, -1, (0, 0, 255), 3)
+    # show_images([questionsContourImage])
+
+    # # Sorting the contours according to the question
+    # questionCnts = contours.sort_contours(questionCnts, method="top-to-bottom")[0]
+    # correct = 0
+    # questionsContourImage = paper.copy()
+
+    # for (q, i) in enumerate(np.arange(0, len(questionCnts), 5)):
+    #     cnts = contours.sort_contours(questionCnts[i: i+5])[0]
+    #     cv2.drawContours(questionsContourImage, cnts, -1, (255,0,0), 2)
+    #     bubbled = None
+
+    #     for (j, c) in enumerate(cnts):
+    #         mask = np.zeros(thresh.shape, dtype="uint8")
+    #         cv2.drawContours(mask, [c], -1, 255, -1)
+
+    #         mask = cv2.bitwise_and(thresh, thresh, mask=mask)
+    #         #show_images([mask])
+    #         total = cv2.countNonZero(mask)
+
+    #         if bubbled is None or total > bubbled[0]:
+    #             bubbled = (total, j)
+
+    #     color = (0, 0, 255)
+    #     k = ANSWER_KEY[q]
+
+    #     if k == bubbled[1]:
+    #         color = (0, 255, 0)
+    #         correct += 1
+
+    #     cv2.drawContours(paper, [cnts[k]], -1, color, 3)
+
+    # score = (correct / 5.0) * 100
+    # print("Score: {:.2f}%".format(score))
+    # cv2.putText(paper, "{:.2f}%".format(score), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+    # show_images([image, paper])
